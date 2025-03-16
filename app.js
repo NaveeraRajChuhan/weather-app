1330195d0206e45ebaeedbe1d014b56357f022f5
NaveeraRajChuhan <nnfstars@gmail.com>
2025-03-16 18:09:15 +0500

    sunny

---
+++ app.js
@@ -0,0 +1,17 @@
+let Weather = prompt("enter your weather")
+
+let sunnyWeather ="sunny"
+let windyWeather ="windy"
+let hotWeather ="hot"
+let coldWeather ="cold"
+let normalWeather ="normal"
+let rainyWeather ="rainy"
+let stormyWeather ="stormy"
+let snoweyWeather ="snoey"
+
+
+
+
+if("sunny"){
+    document.write("<h1>" + sunnyWeather + "</h1>" + "<img src='' alt=''>")
+}
