6d98f547fdacae3f887c23d3c3d9ed5a48259091
NaveeraRajChuhan <nnfstars@gmail.com>
2025-03-16 18:09:36 +0500

    windy

--- app.js
+++ app.js
@@ -14,4 +14,8 @@ let snoweyWeather ="snoey"
 
 if("sunny"){
     document.write("<h1>" + sunnyWeather + "</h1>" + "<img src='' alt=''>")
+}else if ("windy"){
+    document.write("<h1>" + windyWeather + "</h1>" + "<img src='' alt=''>")
+   
+    
 }
